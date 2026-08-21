8c20e91561919c3fb7bb04c6da30df5f4555345d
Ashley <308555169+observabilityengine@users.noreply.github.com>
2026-08-21 11:35:01 -0400

    Build: establish minimal visual system

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,1 +1,17 @@
-export const theme = {} as const;
+export const theme = {
+  colors: {
+    background: '#F1F0EB',
+    text: '#242422',
+    secondaryText: '#77766F',
+    inputBackground: '#E9E7E0',
+  },
+  spacing: {
+    page: 24,
+    itemVertical: 22,
+    input: 16,
+  },
+  typography: {
+    memory: 24,
+    input: 20,
+  },
+};
